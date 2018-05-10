5452f8767610455fbd450cd48d20ff6858dde2a1
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2018-05-10 09:15:00 +0700

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,29 +1,10 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+#include <LiquidCrystal.h>// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup(){// set up the LCD's number of columns and rows: 
+lcd.begin(16,2);// Print a message to the LCD.
+lcd.print("hello, world!");
 }
-
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+void loop(){// set the cursor to column 0, line 1// (note: line 1 is the second row, since counting begins with0 ):
+lcd.setCursor(0,1);// print the number of seconds since reset:
+lcd.print(millis()/1000);
 }
