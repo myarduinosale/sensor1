46e6907e625555a20ffcb2815bebb771190fbddc
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2018-05-23 11:39:38 +0700

    Add files via upload

---
+++ Servo_test_180.ino
@@ -0,0 +1,13 @@
+#include <Servo.h> // ส่งผนวกไฟล์ ชื่อ Servo.h เข้ามาใช้ในโปรแกรม 
+int Servo1 = 9; // กําหนดให้ Servol แทน Pin Digital-9
+Servo myservo; // สร้าง object ชื่อ myservo เพื่อควบคุม Servo
+void setup()
+{
+  myservo.attach(Servo1); // กําหนดให้ใช้ขา Digital-9 สร้างสัญญาณควบคุม Servo
+}
+void loop()
+{
+  myservo.write(180); // กําหนดค่าตําแหน่งให้กับ Servo = 180 องศา
+
+}
+
