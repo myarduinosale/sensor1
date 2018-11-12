620ad641f6f9f6d578c1fef07536ce228cd9620a
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2018-11-12 21:24:43 +0700

    Add files via upload

---
+++ Arduino_OLED128x64_I2C_1.3.ino
@@ -0,0 +1,26 @@
+#include "SPI.h"
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SH1106.h"
+
+#define OLED_RESET 4
+Adafruit_SH1106 display(OLED_RESET);
+
+void setup() {
+display.begin(SH1106_SWITCHCAPVCC, 0x3C);
+display.clearDisplay();
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(0, 5);
+
+display.println("WELCOME to Myarduino");
+display.println();
+display.setTextSize(2);
+display.println("Myarduino");
+display.setTextSize(1);
+display.println("...OLED 1.3 TESTER...");
+display.display();
+delay(5000);
+}
+void loop() {
+}
