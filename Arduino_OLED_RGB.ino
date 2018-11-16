43f8dae25ae1926817609b3fcde2d35e04872a59
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2018-11-16 14:39:58 +0700

    Add files via upload

---
+++ Arduino_OLED_RGB.ino
@@ -0,0 +1,88 @@
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1331.h>
+#include <SPI.h>
+// You can use any (4 or) 5 pins 
+#define sclk 13
+#define mosi 11
+#define cs   10
+#define rst  9
+#define dc   8
+
+
+// Color definitions
+#define	BLACK           0x0000
+#define	BLUE            0x001F
+#define	RED             0xF800
+#define	GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
+// Option 1: use any pins but a little slower
+//Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);  
+
+// Option 2: must use the hardware SPI pins 
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be 
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);
+
+float p = 3.1415926;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("hello!");
+  display.begin();
+
+  Serial.println("init");
+  uint16_t time = millis();
+  display.fillScreen(BLACK);
+  time = millis() - time;
+  
+  Serial.println(time, DEC);
+  delay(500);
+   
+  tftPrintTest();
+  delay(2000);
+
+}
+
+void loop() {
+}
+
+void tftPrintTest() {
+  display.fillScreen(BLACK);
+  display.setCursor(0, 5);
+  display.setTextColor(RED);  
+  display.setTextSize(1);
+  display.println("  myarduino.net");
+  display.setTextColor(YELLOW, GREEN);
+  display.setTextSize(2);
+  display.print("Arduino");
+  display.setTextColor(BLUE);
+  display.setTextSize(3);
+  display.print(1234.567);
+  delay(2000);
+  display.setCursor(0, 5);
+  display.fillScreen(BLACK);
+  display.setTextColor(WHITE);
+  display.setTextSize(0);
+  display.println("Hello World!");
+  display.setTextSize(1);
+  display.setTextColor(GREEN);
+  display.print(p, 5);
+  display.println(" Want pi?");
+  display.print(8675309, HEX); // print 8,675,309 out in HEX!
+  display.print(" Print HEX");
+  display.setTextColor(WHITE);
+  display.println("Sketch has been");
+  display.println("running for: ");
+  display.setTextColor(MAGENTA);
+  display.print(millis() / 1000);
+  display.setTextColor(WHITE);
+  display.print(" seconds.");
+}
+
+
