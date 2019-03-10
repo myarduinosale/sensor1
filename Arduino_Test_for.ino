862ecfea300bdba44720aa5a6b282ae96922350e
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2019-03-10 12:43:41 +0700

    Add files via upload

---
+++ Arduino_Test_for.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Test for myarduino"); // พิมพ์ข้อมความส่งเข้าคอมพิวเตอร์ "Test for myarduino"
+  for (int x = 0; x < 10; x++) { //ให้วนรอบโดยกำหนดให้ X = 0 ถึง 9
+    Serial.print("X = "); // พิมพ์ข้อมความส่งเข้าคอมพิวเตอร์ "X = "
+    Serial.println(x); // พิมพ์ค่าชองตัวแปร x
+    delay(200); //หน่วงเวลา 200mS
+  }
+}
