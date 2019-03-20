a908a04cfb9c1809cbb46bd2234f84ceb32d3385
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2019-03-20 21:09:13 +0700

    Add files via upload

---
+++ Arduino_L298N_LR.ino
@@ -0,0 +1,25 @@
+int MotorPin2 = 2;
+int MotorPin3 = 3;
+void setup() {
+  pinMode(MotorPin2, OUTPUT);
+  pinMode(MotorPin3, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(MotorPin2, HIGH);
+  digitalWrite(MotorPin3, LOW);
+  Serial.println("Motor Right");
+  delay(2000);
+  digitalWrite(MotorPin2, LOW);
+  digitalWrite(MotorPin3, LOW);
+  Serial.println("Motor STOP");
+  delay(2000);
+  digitalWrite(MotorPin2, LOW);
+  digitalWrite(MotorPin3, HIGH);
+  Serial.println("Motor Left");
+  delay(2000);
+
+
+}
