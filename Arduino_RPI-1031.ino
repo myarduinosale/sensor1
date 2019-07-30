2af5499f1b6d40c1c755bb91afa5a4f131bb748d
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2019-07-30 23:39:35 +0700

    Add files via upload

---
+++ Arduino_RPI-1031.ino
@@ -0,0 +1,22 @@
+int s1 = 6;
+int s2 = 7;
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int a = digitalRead(s1);
+  int b = digitalRead(s2);
+  if (a == 0 && b == 0) {
+    Serial.println("A");
+  } else if (a == 1 && b == 0) {
+    Serial.println("B");
+  } else if (a == 1 && b == 1) {
+    Serial.println("C");
+  } else if (a == 0 && b == 1) {
+    Serial.println("D");
+  }
+  delay(200);
+}
