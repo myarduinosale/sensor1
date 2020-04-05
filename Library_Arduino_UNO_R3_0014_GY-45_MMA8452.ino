96bfb4b52993863bac218bcee3068acfebb81310
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2020-04-05 22:59:59 +0700

    Add files via upload

---
+++ Library_Arduino_UNO_R3_0014_GY-45_MMA8452.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+#include <MMA8453_n0m1.h>
+
+MMA8453_n0m1 accel;
+
+void setup()
+{
+  Serial.begin(9600);
+  accel.setI2CAddr(0x1C); //change your device address if necessary, default is 0x1C
+  accel.dataMode(true, 2); //enable highRes 10bit, 2g range [2g,4g,8g]
+  Serial.println("XYZ Data Example");
+}
+
+void loop()
+{
+  accel.update();
+
+  Serial.print("x: ");
+  Serial.print(accel.x());
+  Serial.print(" y: ");
+  Serial.print(accel.y());
+  Serial.print(" z: ");
+  Serial.println(accel.z());
+  delay(500);
+}
