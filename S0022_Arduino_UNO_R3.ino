c22ddb7e71629f28e2f2f607dcc6da60ec5a498a
myarduinosale <37681666+myarduinosale@users.noreply.github.com>
2020-05-09 23:34:03 +0700

    Add files via upload

---
+++ S0022_Arduino_UNO_R3.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+const int pin_RS = 8;
+const int pin_EN = 9;
+const int pin_d4 = 4;
+const int pin_d5 = 5;
+const int pin_d6 = 6;
+const int pin_d7 = 7;
+const int pin_BL = 10;
+LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(3, 0); // กำหนดตำแหน่ง Cursor (ช่องแนวนอนนับจากด้านซ้าย, ฃ่องแนวตั้งนับจากด้านบน)
+  lcd.print("My Arduino");// แสดงข้อความออกจอ LCD
+  lcd.setCursor(0, 1);
+  lcd.print("Press Key:");
+}
+void loop() {
+  int x;
+  x = analogRead (0); // อ่านค่าปุ่มที่ผู้ใช้งานกด
+  lcd.setCursor(10, 1);
+  
+  if (x < 60) {
+    lcd.print ("Right ");
+  }
+  else if (x < 200) {
+    lcd.print ("Up    ");
+  }
+  else if (x < 400) {
+    lcd.print ("Down  ");
+  }
+  else if (x < 600) {
+    lcd.print ("Left  ");
+  }
+  else if (x < 800) {
+    lcd.print ("Select");
+  }
+}
